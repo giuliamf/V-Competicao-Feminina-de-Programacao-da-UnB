9f88dc0e37ba1c457cac452ad85873e1940d4388
Marina <marinamcsouza@gmail.com>
2024-12-15 12:10:48 -0300

    feat: adiciona questao e resolucao B em PYTHON

---
+++ Resolucoes/B-CacaAoTesouro.py
@@ -0,0 +1,9 @@
+# Leitura do input
+N = int(input())  # Número total de ilhas
+s = input()       # Palavra fornecida
+
+# Contagem da letra 'x' na palavra
+quantidade_x = s.count('x')
+
+# Impressão do resultado
+print(quantidade_x)
